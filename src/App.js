41b0858e4064befe9c9bbde671494f13ca8bcdc0
xuliangliang <835547206@qq.com>
2018-11-15 23:14:15 +0800

    修改图片引用

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
                                     <Card
                                         hoverable
                                         style={{ width: '100%'}}
-                                        cover={<img alt="example" style={{ height:'700px'}} src="https://goss.veer.com/creative/vcg/veer/800water/veer-155204544.jpg" />}
+                                        cover={<img alt="example" style={{ height:'700px'}} src="https://grasswort.oss-cn-hangzhou.aliyuncs.com/grasswort/img/girl.jpg" />}
                                     >
                                     </Card>
                                 </div>
@@ -97,7 +97,7 @@ class App extends Component {
                                     <Card
                                         hoverable
                                         style={{ width: '100%'}}
-                                        cover={<img alt="example" style={{ height:'700px'}} src="https://gaopin-img.bj.bcebos.com/133101725027.jpg" />}
+                                        cover={<img alt="example" style={{ height:'700px'}} src="https://grasswort.oss-cn-hangzhou.aliyuncs.com/grasswort/img/girl2.jpg" />}
                                     >
                                     </Card>
                                 </div>
@@ -105,15 +105,7 @@ class App extends Component {
                                     <Card
                                         hoverable
                                         style={{ width: '100%'}}
-                                        cover={<img alt="example" style={{ height:'700px'}} src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1536778670264&di=07c7563309915c3a239345a3820d6f3b&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01cc13554242c80000019ae9e173e9.jpg%401280w_1l_2o_100sh.jpg" />}
-                                    >
-                                    </Card>
-                                </div>
-                                <div>
-                                    <Card
-                                        hoverable
-                                        style={{ width: '100%'}}
-                                        cover={<img alt="example" style={{ height:'700px'}} src="http://pic1.win4000.com/wallpaper/2018-08-24/5b7fa5a5c0104.jpg" />}
+                                        cover={<img alt="example" style={{ height:'700px'}} src="https://grasswort.oss-cn-hangzhou.aliyuncs.com/grasswort/img/girl3.jpg" />}
                                     >
                                     </Card>
                                 </div>
@@ -122,7 +114,7 @@ class App extends Component {
                         <Sider width="20%" className="Content" style={{ background:this.state.themeColor}}>
                             <Layout style={{height:'100%'}}>
                                 <Content style={{ height: '50%',background:this.state.themeColor}}>
-                                    <img alt="example" style={{ margin:'10%',width:'80%',height:'80%'}} src="http://jzbka2015.oss-cn-beijing.aliyuncs.com/2018/1025/5356da1a0aad4612900050edb69fba41.jpg" />
+                                    <img alt="example" style={{ margin:'10%',width:'80%',height:'80%'}} src="https://grasswort.oss-cn-hangzhou.aliyuncs.com/logo/si_ming_yue.jpg" />
                                 </Content>
                                 <Layout style={{ height: '50%'}}>
                                     <Header style={{ height:'10%',background:this.state.themeColor}}></Header>
